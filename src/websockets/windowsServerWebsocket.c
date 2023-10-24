da2da4bdde100c27c866469813352ba83a34adbe
Nullctipus <nullctipus@gmail.com>
2023-10-24 14:28:21 -0500

    extract to functions

--- src/websockets/windowsServerWebsocket.c
+++ src/websockets/windowsServerWebsocket.c
@@ -1,15 +1,18 @@
 #ifdef SERVER_BUILD
 #if _WIN32
 #define WIN32_LEAN_AND_MEAN
-#include "clientWebsocket.h"
 #include <windows.h>
-#include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#else
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#endif
+#include "clientWebsocket.h"
+#include <stdio.h>
 #include <string.h>
 #include "../vector/vector.h"
-
-WSADATA wsadata;
 
 #define WEBSOCKET_BUFF_LEN 2048
 #define NAME_LEN 64
@@ -26,47 +29,57 @@ socklen_t clientAddrLen;
 fd_set activeFdSet;
 fd_set readFdSet;
 
+void close_socket(int fd);
+
 int socketErrorCheck(int returnValue, SOCKET socketToClose, const char *action);
 
 int websocketInitialize()
 {
-    int ret;
-    if ((ret = WSAStartup(MAKEWORD(2, 2), &wsadata)) != 0)
+    int ret = 0;
+
+#ifdef _WIN32
+    WSADATA wsaData;
+    if ((ret = WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0)
     {
         fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
         exit(ret);
     }
     printf("Winsock found!\n"
            "Current status is: %s.\n",
-           wsadata.szSystemStatus);
-    if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wVersion) != 2)
+           wsaData.szSystemStatus);
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
     {
-        printf("This app doesn't support %u.%u!", LOBYTE(wsadata.wVersion),
-               HIBYTE(wsadata.wVersion));
+        printf("This app doesn't support %u.%u!", LOBYTE(wsaData.wVersion),
+               HIBYTE(wsaData.wVersion));
         websocketCleanup();
         exit(ret);
     }
     printf("The dll supports the Winsock version %u.%u!\n",
-           LOBYTE(wsadata.wVersion), HIBYTE(wsadata.wVersion));
+           LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
 
     printf("The highest version this dll can support: %u.%u\n",
-           LOBYTE(wsadata.wHighVersion), HIBYTE(wsadata.wHighVersion));
+           LOBYTE(wsaData.wHighVersion), HIBYTE(wsaData.wHighVersion));
+
+    setvbuf(stdout, NULL, _IONBF, 0);
+#endif
+
     recbuff = malloc(WEBSOCKET_BUFF_LEN * sizeof(char));
     return ret;
 }
 int websocketCleanup()
 {
-    int ret;
+    int ret = 0;
+#ifdef _WIN32
     if ((ret = WSACleanup()) == SOCKET_ERROR)
     {
         fprintf(stderr, "WSACleanup failed with error %d\n", WSAGetLastError());
     }
+#endif
     free(recbuff);
     return ret;
 }
-void websocketStart(int port)
+void createWebsocket(int port)
 {
-
     serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (serverSocket == INVALID_SOCKET)
     {
@@ -89,6 +102,83 @@ void websocketStart(int port)
 
     FD_ZERO(&activeFdSet);
     FD_SET(serverSocket, &activeFdSet);
+}
+void sendAll(char *data, SOCKET exclude)
+{
+    size_t buffsize = strlen(data);
+
+    for (int j = 0; j < clients.length; j++)
+    {
+        SOCKET sock = *(SOCKET *)Vector_At(&clients, j);
+        if (sock == serverSocket || sock == exclude)
+            continue;
+        int iRes = send(sock, data, buffsize, 0);
+        if (socketErrorCheck(iRes, sock, "send", 0))
+        {
+            Vector_RemoveAt(&clients, j, 0);
+        }
+    }
+}
+void createConnection()
+{
+    clientAddrLen = sizeof(clientAddr);
+    clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        printf("accept failed with error: %d\n", WSAGetLastError());
+        close_socket(clientSocket);
+        return;
+    }
+    printf("Got connection with uid: %u\n", clientSocket);
+    FD_SET(clientSocket, &activeFdSet);
+
+    Vector_PushBack(&clients, &clientSocket);
+}
+void clientDisconnect(SOCKET currSocket, int index)
+{
+    printf("connection with socket &d closing\n", currSocket);
+    int iRes = shutdown(currSocket, SD_SEND);
+    if (!socketErrorCheck(iRes, currSocket, "shutdown", 0))
+        close_socket(currSocket);
+
+    FD_CLR(currSocket, &activeFdSet);
+    Vector_RemoveAt(&clients, index, 0);
+    sprintf(recbuff, "USERLEFT;%u\n", currSocket);
+    sendAll(recbuff, -1);
+}
+void dataReceived(SOCKET currSocket, int index, char *type, char *value)
+{
+    int iRes;
+    if (strcmp(type, "ping") == 0)
+    {
+        iRes = send(currSocket, "pong\n", 5, 0);
+        socketErrorCheck(iRes, currSocket, "send", 0);
+        return;
+    }
+    if (strcmp(type, "join") == 0)
+    {
+
+        sprintf_s(recbuff, 7 + NAME_LEN, "join;%s\n", value);
+        int slen = strlen(recbuff);
+        for (int j = 0; j < clients.length; j++)
+        {
+            SOCKET sock = *(SOCKET *)Vector_At(&clients, j);
+            if (serverSocket == sock || currSocket == sock)
+                continue;
+            iRes = send(sock, recbuff, slen, 0);
+            if (socketErrorCheck(iRes, sock, "send", 0))
+            {
+                Vector_RemoveAt(&clients, j--, 0);
+            }
+        }
+        return;
+    }
+}
+void websocketStart(int port)
+{
+
+    int iRes;
+    createWebsocket(port);
 
     clients = Vector_Create(sizeof(SOCKET));
     Vector_PushBack(&clients, &serverSocket);
@@ -113,18 +203,7 @@ void websocketStart(int port)
         }
         if (currSocket == serverSocket) // new connection
         {
-            clientAddrLen = sizeof(clientAddr);
-            clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-            if (clientSocket == INVALID_SOCKET)
-            {
-                printf("accept failed with error: %d\n", WSAGetLastError());
-                closesocket(clientSocket);
-                continue;
-            }
-            printf("Got connection with uid: %u\n", clientSocket);
-            FD_SET(clientSocket, &activeFdSet);
-
-            Vector_PushBack(&clients, &clientSocket);
+            createConnection();
             continue;
         }
 
@@ -134,34 +213,14 @@ void websocketStart(int port)
         if (iRes < 0)
         {
             printf("recv failed: %d\n", WSAGetLastError());
-            closesocket(currSocket);
+            close_socket(currSocket);
             FD_CLR(currSocket, &activeFdSet);
             Vector_RemoveAt(&clients, i, 0);
             continue;
         }
         if (iRes == 0)
         {
-            printf("connection with socket &d closing\n", currSocket);
-            iRes = shutdown(currSocket, SD_SEND);
-            if (!socketErrorCheck(iRes, currSocket, "shutdown", 0))
-                closesocket(currSocket);
-
-            FD_CLR(currSocket, &activeFdSet);
-            Vector_RemoveAt(&clients, i, 0);
-            sprintf(recbuff, "USERLEFT;%u\n", currSocket);
-            size_t buffsize = strlen(recbuff);
-
-            for (int j = 0; j < clients.length; j++)
-            {
-                SOCKET sock = *(SOCKET *)Vector_At(&clients, j);
-                if (serverSocket == sock)
-                    continue;
-                iRes = send(sock, recbuff, buffsize, 0);
-                if (socketErrorCheck(iRes, sock, "send", 0))
-                {
-                    Vector_RemoveAt(&clients, j, 0);
-                }
-            }
+            clientDisconnect(currSocket, i);
             continue;
         }
 
@@ -176,30 +235,7 @@ void websocketStart(int port)
             *value = 0;
             value++;
         }
-        if (strcmp(type, "ping") == 0)
-        {
-            iRes = send(currSocket, "pong\n", 5, 0);
-            socketErrorCheck(iRes, currSocket, "send", 0);
-            continue;
-        }
-        if (strcmp(type, "join") == 0)
-        {
-
-            sprintf_s(recbuff, 7 + NAME_LEN, "join;%s\n", value);
-            int slen = strlen(recbuff);
-            for (int j = 0; j < clients.length; j++)
-            {
-                SOCKET sock = *(SOCKET *)Vector_At(&clients, j);
-                if (serverSocket == sock || currSocket == sock)
-                    continue;
-                iRes = send(sock, recbuff, slen, 0);
-                if (socketErrorCheck(iRes, sock, "send", 0))
-                {
-                    Vector_RemoveAt(&clients, j--, 0);
-                }
-            }
-            continue;
-        }
+        dataReceived(currSocket, i, type, value);
     }
 }
 
@@ -210,12 +246,22 @@ int socketErrorCheck(int returnValue, SOCKET socketToClose, const char *action, 
         return 0;
 
     printf("socket error. %s failed with error: %d\n", actionAttempted, WSAGetLastError());
-    closesocket(socketToClose);
+    close_socket(socketToClose);
     if (!critical)
         return 1;
+#if _WIN32
     WSACleanup();
+#endif
     exit(1);
 }
-
+void close_socket(int fd)
+{
+#ifndef _WIN32
+    shutdown(fd, SHUT_RDWR);
+    close(fd);
+#else
+    closesocket(fd);
 #endif
+}
+
 #endif
